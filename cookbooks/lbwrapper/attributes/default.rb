fbc6eb7cd017b7bea584c6baef1230adbcdce63e
Edwin <edds718@gmail.com>
2014-02-05 14:53:48 -0500

    fix node reference

--- cookbooks/lbwrapper/attributes/default.rb
+++ cookbooks/lbwrapper/attributes/default.rb
@@ -1,9 +1,9 @@
 if node[:cloud][:provider] = "amazon"
   default[:lb][:provider]='lb_elb'
-  default[:lb][:service_name]=node[:lbwrapper][:amazon][:service_name]
+  default[:lb][:service_name]="#{node[:lbwrapper][:amazon][:service_name]}"
 end
 
 if node[:cloud][:provider] = "google"
   default[:lb][:provider]='gcelb_lb'
-  default[:lb][:service_name]=node[:lbwrapper][:gce][:service_name]
+  default[:lb][:service_name]="#{node[:lbwrapper][:gce][:service_name]}"
 end
