1896ee1a2329b3b210eb7c16b0f71c1d3525750a
Edwin <edds718@gmail.com>
2014-02-07 12:31:00 -0500

    test

--- cookbooks/lbwrapper/recipes/lb_connect.rb
+++ cookbooks/lbwrapper/recipes/lb_connect.rb
@@ -5,8 +5,6 @@ if node[:cloud][:provider] = "amazon"
 end
 
 if node[:cloud][:provider] = "google"
-  default[:google_cloud][:project]="#{ENV['GCE_NUMERIC_PROJECT_ID']}"
-  default[:google_cloud][:instance_id="#{ENV['GCE_INSTANCE_ID']}"
-  include_recipe "rsc_google_cloud::default"
+  include_recipe "lb::default"
   include_recipe "lb::lb_do_attach"
 end
