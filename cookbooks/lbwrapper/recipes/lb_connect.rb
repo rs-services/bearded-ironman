34ff0575d833bf424bfc52710128abfb35a34222
Edwin <edds718@gmail.com>
2014-02-07 10:45:03 -0500

    test

--- cookbooks/lbwrapper/recipes/lb_connect.rb
+++ cookbooks/lbwrapper/recipes/lb_connect.rb
@@ -4,6 +4,7 @@ end
 
 if node[:cloud][:provider] = "google"
   include_recipe "rsc_google_cloud::default"
-  include_recipe "rsc_google_cloud::lb_do_attach"
+  include_recipe "lb::lb_do_attach"
+  #include_recipe "rsc_google_cloud::lb_do_attach"
   Chef::Log.info("node[:rightscale][:instance_uuid]")
 end
