de808e8fc35f7dd93fea8e7dd6b8685e95b40024
Edwin <edds718@gmail.com>
2014-02-11 16:37:16 -0500

    testing aws

--- cookbooks/lbwrapper/recipes/lb_connect.rb
+++ cookbooks/lbwrapper/recipes/lb_connect.rb
@@ -3,6 +3,8 @@ require '/var/spool/cloud/meta-data.rb'
 if node[:cloud][:provider] = "amazon"
    #include_recipe "lb::do_attach_request"
    Chef.Log.info('Amazon Cloud Detected')
+   include_recipe "lb::default"
+   include_recipe "lb::do_attach_request"
 end
 
 if node[:cloud][:provider] = "google"
