7374c52e6505b9910a21986430abec1cf888d706
Richard Shade <rshade@rightscale.com>
2014-02-19 15:17:29 -0600

    adding code for logging

--- cookbooks/lbwrapper/metadata.rb
+++ cookbooks/lbwrapper/metadata.rb
@@ -5,9 +5,11 @@ description      "Installs/Configures lbwrapper"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "rightscale"
 depends "lb"
-#depends "google_cloud"
-#depends "rsc_google_cloud"
+depends "lb_elb"
+depends "google_cloud"
+depends "rsc_google_cloud"
 
 recipe "lbwrapper::lb_connect", "connect instance to the load balancer"
 recipe "lbwrapper::lb_disconnect", "disconnect instance from the load balancer"
--- cookbooks/lbwrapper/recipes/lb_connect.rb
+++ cookbooks/lbwrapper/recipes/lb_connect.rb
@@ -1,13 +1,6 @@
 require '/var/spool/cloud/meta-data.rb'
 
-if node[:cloud][:provider] = "amazon"
-   #include_recipe "lb::do_attach_request"
-   Chef::Log.info('Amazon Cloud Detected')
-   include_recipe "lb::default"
-   include_recipe "lb::do_attach_request"
-end
-
-if node[:cloud][:provider] = "google"
-  #include_recipe "rsc_google_cloud::default"
-  #include_recipe "rsc_google_cloud::lb_do_attach"
-end
+log "Cloud Detected: #{node[:cloud][:provider]}"
+log "LB Provider: #{node[:lb][:provider]}"
+include_recipe "lb::default"
+include_recipe "lb::do_attach_request"
