8486d04e3117ad17386ce8c81db044d82328b885
Richard Shade <rshade@rightscale.com>
2014-02-21 16:17:11 -0600

    putting the right provider in

--- cookbooks/lbwrapper/attributes/default.rb
+++ cookbooks/lbwrapper/attributes/default.rb
@@ -1,9 +1,11 @@
 if node[:cloud][:provider] = "amazon"
   default[:lb][:provider]='lb_elb'
+  default[:lb][:service][:provider]='lb_elb'
   default[:lb][:service_name]="Amazon ELB"
 end
 
 if node[:cloud][:provider] = "google"
   default[:lb][:provider]='google_cloud_lb'
+  default[:lb][:service][:provider]='google_cloud_lb'
   default[:lb][:service_name]="Google Load Balancer"
 end
