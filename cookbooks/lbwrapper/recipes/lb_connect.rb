fd5c0aba93d6e5907568be2da0fd121f8ae5b08f
Richard Shade <rshade@rightscale.com>
2014-02-21 15:48:54 -0600

    removing lb::default

--- cookbooks/lbwrapper/recipes/lb_connect.rb
+++ cookbooks/lbwrapper/recipes/lb_connect.rb
@@ -2,5 +2,4 @@ require '/var/spool/cloud/meta-data.rb'
 
 log "Cloud Detected: #{node[:cloud][:provider]}"
 log "LB Provider: #{node[:lb][:provider]}"
-include_recipe "lb::default"
 include_recipe "lb::do_attach_request"
