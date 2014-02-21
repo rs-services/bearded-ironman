4b8948c29dfbb8fdb1db1b580bff659835f453ec
Richard Shade <rshade@rightscale.com>
2014-02-21 14:35:41 -0600

    adding recipes and logs

--- cookbooks/lbwrapper/recipes/default.rb
+++ cookbooks/lbwrapper/recipes/default.rb
@@ -6,3 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+rightscale_marker :begin
+
+log "Cloud Provider: #{node[:cloud][:provider]}"
+log "LB Provider: #{node[:lb][:provider]}"
+log "LB Service Name: #{node[:lb][:service_name]}"
+
+rightscale_marker :end
