3fc6442f996f53a310a181652a152028d3fc0882
Richard Shade <rshade@rightscale.com>
2014-02-24 14:18:06 -0600

    removing remote calls

--- cookbooks/lbwrapper/recipes/lb_connect.rb
+++ cookbooks/lbwrapper/recipes/lb_connect.rb
@@ -2,4 +2,24 @@ require '/var/spool/cloud/meta-data.rb'
 
 log "Cloud Detected: #{node[:cloud][:provider]}"
 log "LB Provider: #{node[:lb][:provider]}"
-include_recipe "lb::do_attach_request"
+
+class Chef::Recipe
+  include RightScale::App::Helper
+end
+
+# Calls the "attach" action for all pools.
+log " Remote recipe executed by do_attach_request"
+# See cookbooks/app/libraries/helper.rb for the "pool_names" method.
+pool_names(node[:lb][:pools]).each do |pool_name|
+  log " Adding tag to answer for vhost load balancing - #{pool_name}."
+  # See cookbooks/lb/definitions/lb_tag.rb for the "lb_tag" definition.
+  lb_tag pool_name
+  # See cookbooks/lb_<provider>/providers/default.rb for the "attach" action.
+  lb pool_name do
+    backend_id node[:remote_recipe][:backend_id]
+    backend_ip node[:remote_recipe][:backend_ip]
+    backend_port node[:remote_recipe][:backend_port].to_i
+    session_sticky node[:lb][:session_stickiness]
+    action :attach
+  end
+end
--- cookbooks/lbwrapper/recipes/lb_disconnect.rb
+++ cookbooks/lbwrapper/recipes/lb_disconnect.rb
@@ -1,12 +1,25 @@
 require '/var/spool/cloud/meta-data.rb'
 
-if node[:cloud][:provider] = "amazon"
-   Chef::Log.info('Amazon Cloud Detected')
-   include_recipe "lb::default"
-   include_recipe "lb::do_detach_request"
+log "Cloud Detected: #{node[:cloud][:provider]}"
+log "LB Provider: #{node[:lb][:provider]}"
+
+class Chef::Recipe
+  include RightScale::App::Helper
 end
 
-if node[:cloud][:provider] = "google"
-  #include_recipe "rsc_google_cloud::default"
-  #include_recipe "rsc_google_cloud::lb_do_attach"
+# Calls the "attach" action for all pools.
+log " Remote recipe executed by do_attach_request"
+# See cookbooks/app/libraries/helper.rb for the "pool_names" method.
+pool_names(node[:lb][:pools]).each do |pool_name|
+  log " Adding tag to answer for vhost load balancing - #{pool_name}."
+  # See cookbooks/lb/definitions/lb_tag.rb for the "lb_tag" definition.
+  lb_tag pool_name
+  # See cookbooks/lb_<provider>/providers/default.rb for the "attach" action.
+  lb pool_name do
+    backend_id node[:remote_recipe][:backend_id]
+    backend_ip node[:remote_recipe][:backend_ip]
+    backend_port node[:remote_recipe][:backend_port].to_i
+    session_sticky node[:lb][:session_stickiness]
+    action :detach
+  end
 end
