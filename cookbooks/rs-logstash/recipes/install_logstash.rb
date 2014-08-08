68e0be0813889af7a33f3f6caee24c7b97de2215
Edwin <edds718@gmail.com>
2014-08-08 13:22:29 -0400

    dropping community cookbooks

--- cookbooks/rs-logstash/recipes/install_logstash.rb
+++ cookbooks/rs-logstash/recipes/install_logstash.rb
@@ -11,7 +11,7 @@
 bash "install_logstash_source" do 
 	cwd "/usr/local/src"
 	code <<-EOH
-	url -O https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
+	curl -O https://download.elasticsearch.org/logstash/logstash/logstash-1.4.2.tar.gz
 	tar zxvf logstash-1.4.2.tar.gz
 	for i in `ls /usr/local/src/logstash-1.4.2/bin/`; do ln -s /usr/local/src/logstash-1.4.2/bin/$i /usr/local/bin/$i;done
 	EOH
