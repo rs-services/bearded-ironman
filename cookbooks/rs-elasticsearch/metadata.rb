cab2974fab3acd62b27425db20001eef8aeeead9
Edwin <edds718@gmail.com>
2014-08-27 10:59:43 -0400

    adding startup scripts

--- cookbooks/rs-elasticsearch/metadata.rb
+++ cookbooks/rs-elasticsearch/metadata.rb
@@ -6,4 +6,6 @@ description      'Installs/Configures rs-elasticsearch'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+recipe "rs-elasticsearch::default", "Installs elasticsearch dependencies."
 recipe "rs-elasticsearch::install_elasticsearch", "Installs elasticsearch for use with logstash"
+recipe "rs-elasticsearch::start_elasticsearch", "Starts elasticsearch daemonize, start in the background"
--- cookbooks/rs-logstash/metadata.rb
+++ cookbooks/rs-logstash/metadata.rb
@@ -6,5 +6,5 @@ description      'Installs/Configures rs-logstash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-
 recipe "rs-logstash::install_logstash", "Installs the logstash server"
+recipe "rs-logstash::start_logstash", "Starts the logstash server"
