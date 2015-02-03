d716226c9a9238c4a901af0d97a74009de7f314d
edwin <edds718@gmail.com>
2015-02-03 18:01:27 -0500

    [x] Backlog Development - MongoDB

--- cookbooks/rs-dns/attributes/default.rb
+++ cookbooks/rs-dns/attributes/default.rb
@@ -1,8 +1,10 @@
 node[:dns][:provider] - dns provider name used by fog (http://fog.io/about/provider_documentation.html)
 # Examples -AWS, DNSMadeEasy, Dynect, Google, Rackspace, Zerigo, OpenStack, Cloudstack
 
-node[:dns][:domain] - domain of the record
-node[:dns][:credentials] - hash of connection credentials used by fog (https://github.com/fog/fog/blob/master/lib/fog/{provider}/dns.rb)
+node[:dns][:domain]="AWS"
+node[:dns][:credentials]
+#- hash of connection credentials used by fog (https://github.com/fog/fog/blob/master/lib/fog/{provider}/dns.rb)
+# https://github.com/fog/fog/blob/master/lib/fog/{provider}/dns.rb
 node[:dns][:disable] - disable creation of node dns record but allow updating if record exists
 node[:dns][:entry][:name] - dns entry name
 node[:dns][:entry][:type] - dns entry type
--- cookbooks/rs-dns/metadata.rb
+++ cookbooks/rs-dns/metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures rs-dns'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 
+depends 'dns', '~> 0.1.3'
