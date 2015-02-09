2f6bf53f7626746c956d87bec4f8bf8ed7652428
edwin <edds718@gmail.com>
2015-02-09 18:09:42 -0500

    start

--- cookbooks/rs-mongodb/metadata.rb
+++ cookbooks/rs-mongodb/metadata.rb
@@ -1,7 +1,10 @@
 name             'rs-mongodb'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'RightScale'
+maintainer_email 'edwin@rightscale.com'
 license          'All rights reserved'
 description      'Installs/Configures rs-mongodb'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+
+depends 'mongodb'
